b8e5b239522a873b3ac42d2dcb0eb6ee3631771b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-15 17:17:26 -0600

    Feature: Added Vector implementation
    This struct will be useful for several graphics operations.
    
    Contact: Sam Christy <samchristywork@gmail.com>

---
+++ src/vector.rs
@@ -0,0 +1,57 @@
+#[derive(Copy, Clone, Debug)]
+pub struct Vector {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl std::ops::Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl std::ops::Sub for Vector {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vector {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+
+impl std::ops::Mul for Vector {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        }
+    }
+}
+
+impl Vector {
+    #[must_use]
+    pub fn rotate(self, f: f32) -> Self {
+        Self {
+            x: self.x * f.cos() - self.y * f.sin(),
+            y: self.y.mul_add(f.cos(), self.x * f.sin()),
+        }
+    }
+}
