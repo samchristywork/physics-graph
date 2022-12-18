a33631fb825be9f1d0af8712fcd7d99c83a0120c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-17 19:39:24 -0600

    Feature: Add text extent inference
    Text extents are now calculated with the `rusttype` library. This
    information is used to create nodes that look nice.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/svg.rs
+++ src/svg.rs
@@ -60,19 +60,57 @@ pub fn draw_label_multiline(
     font_color: &str,
     label: &str,
 ) -> String {
-    let mut y = y;
-    let mut svg = String::new();
+    let mut svg_text = String::new();
+    let mut svg_background = String::new();
+
+    let line_height = font_size + 0.01;
+    let mut label_width = 0.0;
+    let mut label_height = 0.0;
 
     for line in label.split("\\n") {
-        svg += format!(
+        let font_data = include_bytes!("../LiberationSans-Regular.ttf");
+        let font = Font::try_from_bytes(font_data as &[u8]).expect("Error constructing Font");
+
+        let scale = Scale::uniform(32.0);
+
+        let glyphs: Vec<_> = font.layout(line, scale, point(0.0, 0.0)).collect();
+        let text_width = {
+            let min_x = glyphs
+                .first()
+                .map(|g| g.pixel_bounding_box().unwrap().min.x)
+                .unwrap();
+            let max_x = glyphs
+                .last()
+                .map(|g| g.pixel_bounding_box().unwrap().max.x)
+                .unwrap();
+            (max_x - min_x) as f32 / 600.0 + 0.02
+        };
+
+        if label_width < text_width {
+            label_width = text_width;
+        }
+
+        let y = y + label_height;
+        svg_text += format!(
             "<text x=\"{x}\" y=\"{y}\" text-anchor=\"middle\" font-size=\"{font_size}\" \
             fill=\"{font_color}\">{line}</text>"
         )
         .as_str();
-        y += font_size;
+
+        label_height += line_height;
     }
 
-    svg
+    let label_background_x = x - label_width / 2.0;
+    let label_background_y = y - line_height + 0.015;
+
+    svg_background += format!(
+        "<rect x=\"{label_background_x}\" y=\"{label_background_y}\" \
+        width=\"{label_width}\" height=\"{label_height}\" \
+            style=\"fill:white;stroke-width:0.001;stroke:black\"></rect>"
+    )
+    .as_str();
+
+    svg_background + svg_text.as_str()
 }
 
 #[must_use]
